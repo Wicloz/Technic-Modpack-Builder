9fb108d04ee38a0de12233f36895893bcadaab8d
Wicloz <deboer.wilco@gmail.com>
2015-01-19 11:55:29 +0100

    Created a modpack

--- src/bin/Debug/plugins/server_template/RUN.bat
+++ src/bin/Debug/plugins/server_template/RUN.bat
@@ -4,10 +4,11 @@ robocopy world backups\backup_before /e
 
 start /min BackUpper.bat
 ECHO Starting server ...
-java -Xmx3G -Xms2G -XX:PermSize=128m -jar modpack.jar nogui
+java -Xmx3G -Xms2G -XX:PermSize=256m -jar modpack.jar nogui
 
 ECHO Backing up world ...
 rmdir backups\backup_after /Q /S
 robocopy world backups\backup_after /e
 
+pause
 exit
