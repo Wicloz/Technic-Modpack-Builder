72ef90839d807b28c9355d57bbf74d26c455ce20
Wicloz <deboer.wilco@gmail.com>
2015-07-13 20:13:59 +0200

    Modlisting is now done

--- src/ModManager.Designer.cs
+++ src/ModManager.Designer.cs
@@ -28,11 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModManager));
+            this.modListView = new System.Windows.Forms.ListView();
+            this.modName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.uriState = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.versionLocal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.versionLatest = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.updateState = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.updateListButton = new System.Windows.Forms.Button();
+            this.modInfoBox = new System.Windows.Forms.GroupBox();
+            this.linkStatusLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.modDownloadBox = new System.Windows.Forms.TextBox();
+            this.modSiteBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.modNameBox = new System.Windows.Forms.TextBox();
+            this.modInfoBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // modListView
+            // 
+            this.modListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.modListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.modName,
+            this.uriState,
+            this.versionLocal,
+            this.versionLatest,
+            this.updateState});
+            this.modListView.FullRowSelect = true;
+            this.modListView.GridLines = true;
+            this.modListView.HideSelection = false;
+            this.modListView.Location = new System.Drawing.Point(12, 167);
+            this.modListView.MultiSelect = false;
+            this.modListView.Name = "modListView";
+            this.modListView.Size = new System.Drawing.Size(638, 414);
+            this.modListView.TabIndex = 0;
+            this.modListView.UseCompatibleStateImageBehavior = false;
+            this.modListView.View = System.Windows.Forms.View.Details;
+            this.modListView.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.modListView_ItemSelectionChanged);
+            // 
+            // modName
+            // 
+            this.modName.Text = "Mod Name";
+            this.modName.Width = 202;
+            // 
+            // uriState
+            // 
+            this.uriState.Text = "URI State";
+            this.uriState.Width = 86;
+            // 
+            // versionLocal
+            // 
+            this.versionLocal.Text = "Local Version";
+            this.versionLocal.Width = 81;
+            // 
+            // versionLatest
+            // 
+            this.versionLatest.Text = "Latest Version";
+            this.versionLatest.Width = 82;
+            // 
+            // updateState
+            // 
+            this.updateState.Text = "Update State";
+            this.updateState.Width = 114;
+            // 
+            // updateListButton
+            // 
+            this.updateListButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.updateListButton.Location = new System.Drawing.Point(13, 122);
+            this.updateListButton.Name = "updateListButton";
+            this.updateListButton.Size = new System.Drawing.Size(637, 39);
+            this.updateListButton.TabIndex = 1;
+            this.updateListButton.Text = "Update Modlist";
+            this.updateListButton.UseVisualStyleBackColor = true;
+            this.updateListButton.Click += new System.EventHandler(this.updateListButton_Click);
+            // 
+            // modInfoBox
+            // 
+            this.modInfoBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.modInfoBox.Controls.Add(this.linkStatusLabel);
+            this.modInfoBox.Controls.Add(this.label3);
+            this.modInfoBox.Controls.Add(this.modDownloadBox);
+            this.modInfoBox.Controls.Add(this.modSiteBox);
+            this.modInfoBox.Controls.Add(this.label2);
+            this.modInfoBox.Controls.Add(this.label1);
+            this.modInfoBox.Controls.Add(this.modNameBox);
+            this.modInfoBox.Location = new System.Drawing.Point(13, 13);
+            this.modInfoBox.Name = "modInfoBox";
+            this.modInfoBox.Size = new System.Drawing.Size(637, 103);
+            this.modInfoBox.TabIndex = 2;
+            this.modInfoBox.TabStop = false;
+            this.modInfoBox.Text = "Mod Information";
+            // 
+            // linkStatusLabel
+            // 
+            this.linkStatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkStatusLabel.AutoSize = true;
+            this.linkStatusLabel.Location = new System.Drawing.Point(563, 48);
+            this.linkStatusLabel.Name = "linkStatusLabel";
+            this.linkStatusLabel.Size = new System.Drawing.Size(68, 13);
+            this.linkStatusLabel.TabIndex = 6;
+            this.linkStatusLabel.Text = "Unsupported";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Download Link:";
+            // 
+            // modDownloadBox
+            // 
+            this.modDownloadBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.modDownloadBox.Location = new System.Drawing.Point(97, 71);
+            this.modDownloadBox.Name = "modDownloadBox";
+            this.modDownloadBox.Size = new System.Drawing.Size(534, 20);
+            this.modDownloadBox.TabIndex = 4;
+            // 
+            // modSiteBox
+            // 
+            this.modSiteBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.modSiteBox.Location = new System.Drawing.Point(97, 45);
+            this.modSiteBox.Name = "modSiteBox";
+            this.modSiteBox.Size = new System.Drawing.Size(460, 20);
+            this.modSiteBox.TabIndex = 3;
+            this.modSiteBox.TextChanged += new System.EventHandler(this.modSiteBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Website Link:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Name:";
+            // 
+            // modNameBox
+            // 
+            this.modNameBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.modNameBox.Location = new System.Drawing.Point(97, 19);
+            this.modNameBox.Name = "modNameBox";
+            this.modNameBox.Size = new System.Drawing.Size(534, 20);
+            this.modNameBox.TabIndex = 0;
+            // 
+            // ModManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ModManager";
+            this.ClientSize = new System.Drawing.Size(662, 593);
+            this.Controls.Add(this.modInfoBox);
+            this.Controls.Add(this.updateListButton);
+            this.Controls.Add(this.modListView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ModManager";
+            this.Text = "Mod Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ModManager_FormClosing);
+            this.modInfoBox.ResumeLayout(false);
+            this.modInfoBox.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView modListView;
+        private System.Windows.Forms.ColumnHeader modName;
+        private System.Windows.Forms.ColumnHeader uriState;
+        private System.Windows.Forms.ColumnHeader versionLocal;
+        private System.Windows.Forms.ColumnHeader versionLatest;
+        private System.Windows.Forms.ColumnHeader updateState;
+        private System.Windows.Forms.Button updateListButton;
+        private System.Windows.Forms.GroupBox modInfoBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox modNameBox;
+        private System.Windows.Forms.TextBox modSiteBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox modDownloadBox;
+        private System.Windows.Forms.Label linkStatusLabel;
     }
 }
--- src/ModManager.cs
+++ src/ModManager.cs
@@ -7,14 +7,192 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Technic_Modpack_Creator
 {
     public partial class ModManager : Form
     {
+        private List<ModInfo> modList = new List<ModInfo>();
+        private string cd = Directory.GetCurrentDirectory();
+
+        private string selectedModName = "";
+        private int selectedIndex = -1;
+
         public ModManager()
         {
             InitializeComponent();
+            LoadModList();
+            UpdateModList();
+        }
+
+        private void ModManager_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveModList();
+        }
+
+        private void LoadModList()
+        {
+            modList = SaveLoad.LoadFileBf<List<ModInfo>>(cd + "\\settings\\modlist.dat");
+            if (modList == null)
+            {
+                modList = new List<ModInfo>();
+            }
+        }
+
+        private void SaveModList()
+        {
+            SaveLoad.SaveFileBf(modList, cd + "\\settings\\modlist.dat");
+        }
+
+        private void UpdateModList()
+        {
+            List<string> fileList = new List<string>();
+
+            foreach (string file in Directory.GetFiles(cd + "\\modpack\\mods", "*.jar", SearchOption.TopDirectoryOnly))
+            {
+                fileList.Add(Path.GetFileName(file));
+            }
+            foreach (string file in Directory.GetFiles(cd + "\\modpack\\mods", "*.zip", SearchOption.TopDirectoryOnly))
+            {
+                fileList.Add(Path.GetFileName(file));
+            }
+
+            foreach (string file in fileList)
+            {
+                bool exists = false;
+                foreach (ModInfo info in modList)
+                {
+                    if (file == info.modFile)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exists)
+                {
+                    modList.Add(new ModInfo(file));
+                }
+            }
+
+            for (int i = 0; i < modList.Count; i++)
+            {
+                bool exists = false;
+                foreach (string file in fileList)
+                {
+                    if (modList[i].modFile == file)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+
+                if (!exists)
+                {
+                    modList.RemoveAt(i);
+                    i--;
+                }
+                else
+                {
+                    modList[i].UpdateModValues();
+                }
+            }
+
+            modList.Sort();
+            UpdateListView();
+            SaveModList();
+        }
+
+        private void UpdateListView()
+        {
+            modListView.Items.Clear();
+
+            foreach (ModInfo mod in modList)
+            {
+                ListViewItem lvi = new ListViewItem(mod.modFile);
+
+                lvi.SubItems.Add(mod.uriState);
+                lvi.SubItems.Add(mod.versionLocal);
+                lvi.SubItems.Add(mod.versionLatest);
+                lvi.SubItems.Add(mod.updateState);
+
+                modListView.Items.Add(lvi);
+            }
+
+            // Selection
+            modListView.SelectedIndices.Clear();
+            int index = 0;
+
+            if (selectedModName != "")
+            {
+                for (int i = 0; i < modListView.Items.Count; i++)
+                {
+                    string item = modListView.Items[i].SubItems[0].Text;
+                    if (item == selectedModName)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+
+            if (index >= 0)
+            {
+                modListView.SelectedIndices.Add(index);
+                SelectItem(index);
+            }
+            else
+            {
+                ClearSelection();
+            }
+        }
+
+        private void UpdateModFields(int index)
+        {
+            ModInfo mod = modList[index];
+
+            modNameBox.Text = mod.modFile;
+            modSiteBox.Text = mod.website;
+            modDownloadBox.Text = mod.dlSite;
+            linkStatusLabel.Text = mod.uriState;
+        }
+
+        private void ClearSelection()
+        {
+            selectedModName = "";
+            selectedIndex = -1;
+
+            modNameBox.Text = "";
+            modSiteBox.Text = "";
+            modDownloadBox.Text = "";
+            linkStatusLabel.Text = "";
+        }
+
+        private void SelectItem(int index)
+        {
+            selectedModName = modListView.Items[index].SubItems[0].Text;
+            selectedIndex = index;
+            UpdateModFields(selectedIndex);
+        }
+
+        private void modListView_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (modListView.SelectedIndices.Count > 0)
+            {
+                SelectItem(modListView.SelectedIndices[0]);
+            }
+        }
+
+        private void updateListButton_Click(object sender, EventArgs e)
+        {
+            UpdateModList();
+        }
+
+        private void modSiteBox_TextChanged(object sender, EventArgs e)
+        {
+            modList[selectedIndex].website = modSiteBox.Text;
+            modListView.Items[selectedIndex].SubItems[1].Text = modList[selectedIndex].uriState;
         }
     }
 }
